d13d7422f2b697688d0c87a957bb706ef5f65077
Vera Kippel <veki2400@student.miun.se>
2026-02-10 10:27:48 +0100

    skapar paths för routing, inkluderar routing i main och skapar en komponent-mapp och Header-fil

--- src/routing.tsx
+++ src/routing.tsx
@@ -2,3 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 import StartPage from "./pages/StartPage";
 import ProductsPage from "./pages/ProductsPage";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <StartPage />
+    },
+    {
+        path: "/products",
+        element: <ProductsPage />
+    }
+])
+
+export default router;
