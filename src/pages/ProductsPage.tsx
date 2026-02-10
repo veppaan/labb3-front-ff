75f47857d33c9ce72d202759e53368f3b986291f
Vera Kippel <veki2400@student.miun.se>
2026-02-10 10:22:46 +0100

    skapar undersidor och en router-fil med importer

---
+++ src/pages/ProductsPage.tsx
@@ -0,0 +1,10 @@
+
+const Products = () => {
+  return (
+    <div>
+        <h1>Produkter</h1>
+    </div>
+  )
+}
+
+export default Products
---
+++ src/routing.tsx
@@ -0,0 +1,4 @@
+import { createBrowserRouter } from "react-router-dom";
+import StartPage from "./pages/StartPage";
+import ProductsPage from "./pages/ProductsPage";
+
